94a764961c50b4ee647ac3a8dea08e4c92d70fea
Rodrigo Caldas <rodrigoacaldas@gmail.com>
2020-03-11 19:50:05 -0300

    Acrescentando lista invertida para o bobblesort e selectionsort

--- bubblesort/bubblesort.py
+++ bubblesort/bubblesort.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from random import randint
 import time
 
-def desenhaGrafico(x, y, xl="Entradas", yl="Saídas"):
+def desenhaGrafico(x, y, xl="Entradas", yl="Saídas", nome="bubblesort"):
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
     ax.plot(x, y, label="Melhor Tempo")
     ax.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
-    fig.savefig('bubblesort.png')
+    fig.savefig(nome+'.png')
 
 def geraLista(tam):
     lista = []
@@ -19,8 +19,15 @@ def geraLista(tam):
         if n not in lista: lista.append(n)
     return lista
 
+def geraListaInvertida(tam):
+    lista_inv = []
+    for i in range(tam):
+        lista_inv.append(tam-i)
+    return lista_inv
+
 tamanhos_listas = [1000,10000,30000,60000]
 tempos = []
+tempos_inv = []
 
 def bublesort(lista):
     agora = time.time()
@@ -34,13 +41,20 @@ def bublesort(lista):
                 lista[j], lista[j+1] = lista[j+1], lista[j]
 
     depois = time.time()
-    print("Loops feitos para a lista com", n, "numeros:", loop)
+    tmp = depois-agora
+    print("Loops feitos para a lista com", n, "numeros:", loop, "e esse proceso demorou:", tmp, "segundos")
 
-    return depois-agora
+    return tmp
 
 for tamanho in tamanhos_listas:
     lista = geraLista(tamanho)
     tempo = bublesort(lista)
     tempos.append(tempo)
 
-desenhaGrafico(tamanhos_listas, tempos, "Tamanho", "Tempo")
+for tamanho in tamanhos_listas:
+    lista = geraListaInvertida(tamanho)
+    tempo_inv = bublesort(lista)
+    tempos_inv.append(tempo_inv)
+
+desenhaGrafico(tamanhos_listas, tempos, "Tamanho", "Tempo", "bubblesortListaAleatoria")
+desenhaGrafico(tamanhos_listas, tempos_inv, "Tamanho", "Tempo", "bubblesortListaInvertida")
--- selectionSort/selectionSort.py
+++ selectionSort/selectionSort.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from random import randint
 import time
 
-def desenhaGrafico(x, y, xl="Entradas", yl="Saídas"):
+def desenhaGrafico(x, y, xl="Entradas", yl="Saídas",name="selectionSort"):
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111)
     ax.plot(x, y, label="Melhor Tempo")
     ax.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
-    fig.savefig('selectionSort.png')
+    fig.savefig(name+'.png')
 
 def geraLista(tam):
     lista = []
@@ -19,8 +19,15 @@ def geraLista(tam):
         if n not in lista: lista.append(n)
     return lista
 
+def geraListaInvertida(tam):
+    lista_inv = []
+    for i in range(tam):
+        lista_inv.append(tam-i)
+    return lista_inv
+
 tamanhos_listas = [1000, 10000, 30000, 60000]
 tempos = []
+tempos_inv = []
 
 def selectionSort(lista):
     agora = time.time()
@@ -38,13 +45,21 @@ def selectionSort(lista):
         lista[i] = atual
 
     depois = time.time()
-    print("Loops feitos para a lista com", n, "numeros:", loop)
+    tmp = depois - agora
+    print("Loops feitos para a lista com", n, "numeros:", loop, "e esse proceso demorou:", tmp, "segundos")
     #print(lista)
-    return depois - agora
+    return tmp
 
 for tamanho in tamanhos_listas:
     lista = geraLista(tamanho)
     tempo = selectionSort(lista)
     tempos.append(tempo)
 
-desenhaGrafico(tamanhos_listas, tempos, "Tamanho", "Tempo")
+desenhaGrafico(tamanhos_listas, tempos, "Tamanho", "Tempo", "selectionSortListaAleatoria")
+
+for tamanho in tamanhos_listas:
+    lista = geraListaInvertida(tamanho)
+    tempo_inv = selectionSort(lista)
+    tempos_inv.append(tempo_inv)
+
+desenhaGrafico(tamanhos_listas, tempos_inv, "Tamanho", "Tempo", "selectionSortListaInvertida")
